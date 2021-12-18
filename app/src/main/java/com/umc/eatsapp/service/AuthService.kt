133beadbdb4e381906f2d845e7cc922fc1617fbf
mimwin <kms105903@naver.com>
2021-12-19 05:39:54 +0900

    add service for auth and store

--- app/src/main/java/com/umc/eatsapp/service/AuthService.kt
+++ app/src/main/java/com/umc/eatsapp/service/AuthService.kt
@@ -1,4 +1,73 @@
 package com.umc.eatsapp.service
 
+import android.util.Log
+import com.umc.eatsapp.AuthResponse
+import com.umc.eatsapp.Interface.AuthRetrofitInterface
+import com.umc.eatsapp.data.User
+import com.umc.eatsapp.getRetrofit
+import com.umc.eatsapp.serviceView.LoginView
+import com.umc.eatsapp.serviceView.RegisterView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class AuthService {
+
+    private lateinit var registerView : RegisterView
+    private lateinit var loginView : LoginView
+
+    private val AuthService = getRetrofit().create(AuthRetrofitInterface::class.java)
+
+    fun setRegisterView(registerView : RegisterView){
+        this.registerView = registerView
+    }
+    fun setLoginView(loginView : LoginView){
+        this.loginView = loginView
+    }
+
+    fun register(user: User){
+
+        registerView.onRegisterLoading()
+
+        AuthService.register(user).enqueue(object : Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                val signUpResponse: AuthResponse = response.body()!!
+                Log.e("SignUP/API", signUpResponse.toString())
+
+                when (signUpResponse.code) {
+                    1000 -> {
+                        registerView.onRegisterSucces()
+                    }
+                    else -> registerView.onRegisterFailure(signUpResponse.code, signUpResponse.message)
+                }
+            }
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                registerView.onRegisterFailure(400, t.message.toString())
+            }
+        })
+    }
+
+    fun login(user : User){
+        loginView.onLoginLoading()
+
+        AuthService.login(user).enqueue(object: Callback<AuthResponse> {
+
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+
+                val LoginResponse: AuthResponse = response.body()!!
+
+                Log.e("LOGIN/API",LoginResponse.toString())
+
+                when(LoginResponse.code){
+                    1000 -> loginView.onLoginSuccess(LoginResponse.result!!)
+                    else -> loginView.onLoginFailure(LoginResponse.code, LoginResponse.message)
+                }
+            }
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                loginView.onLoginFailure(400, t.message.toString())
+            }
+
+        })
+    }
+
 }
--- app/src/main/java/com/umc/eatsapp/service/StoreService.kt
+++ app/src/main/java/com/umc/eatsapp/service/StoreService.kt
@@ -1,4 +1,69 @@
 package com.umc.eatsapp.service
 
+import android.util.Log
+import com.umc.eatsapp.CatResponse
+import com.umc.eatsapp.Interface.AuthRetrofitInterface
+import com.umc.eatsapp.StoreResponse
+import com.umc.eatsapp.getRetrofit
+import com.umc.eatsapp.serviceView.CatView
+import com.umc.eatsapp.serviceView.StoreView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class StoreService {
+    private lateinit var catView : CatView
+    private lateinit var storeView : StoreView
+
+    private val StoreService = getRetrofit().create(AuthRetrofitInterface::class.java)
+
+    fun setCatView(catView : CatView){
+        this.catView = catView
+    }
+    fun setStoreView(storeView : StoreView){
+        this.storeView = storeView
+    }
+
+    fun category(){
+
+        catView.onCatLoading()
+
+        StoreService.category().enqueue(object : Callback<CatResponse> {
+            override fun onResponse(call: Call<CatResponse>, response: Response<CatResponse>) {
+                val catResponse : CatResponse = response.body()!!
+
+                when(catResponse.code){
+                    1000-> catView.onCatSuccess(catResponse.result!!)
+                    else->catView.onCatFailure(catResponse.code, catResponse.message)
+                }
+            }
+
+            override fun onFailure(call: Call<CatResponse>, t: Throwable) {
+                catView.onCatFailure(400,t.message.toString())
+            }
+        })
+    }
+
+    fun store(type : String){
+        storeView.onStoreLoading()
+
+        StoreService.store(type).enqueue(object: Callback<StoreResponse> {
+
+            override fun onResponse(call: Call<StoreResponse>, response: Response<StoreResponse>) {
+
+                val storeResponse: StoreResponse = response.body()!!
+
+                Log.e("LOGIN/API",storeResponse.toString())
+
+                when(storeResponse.code){
+                    1000 -> storeView.onStoreSuccess(storeResponse.result!!)
+                    else -> storeView.onStoreFailure(storeResponse.code, storeResponse.message)
+                }
+            }
+            override fun onFailure(call: Call<StoreResponse>, t: Throwable) {
+                storeView.onStoreFailure(400, t.message.toString())
+            }
+
+        })
+    }
 }
