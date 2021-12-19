becacce66df6481798ecc5e7492bfa371b3d1611
mimwin <kms105903@naver.com>
2021-12-19 15:06:44 +0900

    Myinfo fragment with settings and banner

--- app/src/main/java/com/umc/eatsapp/view/MyinfoFragment.kt
+++ app/src/main/java/com/umc/eatsapp/view/MyinfoFragment.kt
@@ -1,14 +1,23 @@
 package com.umc.eatsapp.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.umc.eatsapp.R
+import com.umc.eatsapp.adapter.homefrag_banner_pageradapter
 import com.umc.eatsapp.databinding.FragmentMyinfoBinding
 
 class MyinfoFragment : Fragment() {
     lateinit var binding : FragmentMyinfoBinding
+
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var banner : Banner
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +25,38 @@ class MyinfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMyinfoBinding.inflate(inflater,container,false)
+        binding.myinfoScrollview.fullScroll(ScrollView.FOCUS_DOWN);
+        initBanner()
+
         return binding.root
+    }
+
+    private fun initBanner(){
+        val bannerAdapter = homefrag_banner_pageradapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_homefrag_1))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_homefrag_2))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_homefrag_3))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_homefrag_4))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_homefrag_5))
+
+        binding.myinfoBannerVp.adapter = bannerAdapter
+        binding.myinfoBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        banner = Banner(0)
+        banner.start()
+    }
+
+    inner class Banner(private var currentPos : Int) : Thread(){
+        override fun run() {
+            while(true){
+                sleep(5000)
+                handler.post {
+                    if(currentPos==5) currentPos=0
+                    binding.bannerNum.text = (currentPos+1).toString() + " / 5"
+                    binding.myinfoBannerVp.setCurrentItem(currentPos,true)
+                    currentPos+=1
+                }
+            }
+        }
     }
 }
